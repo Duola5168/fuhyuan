13f557791678daf46edc40f2fddda32e5862b0fb
Duola5168 <qszxc794613258@gmail.com>
2025-07-11 16:11:41 +0800

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,8 +1,17 @@
+
+export interface ProductItem {
+  id: string;
+  name: string;
+  quantity: number;
+  serialNumber: string;
+}
+
 export interface WorkOrderData {
   dateTime: string;
   serviceUnit: string;
   contactPerson: string;
   contactPhone: string;
+  products: ProductItem[];
   tasks: string;
   status: string;
   remarks: string;
